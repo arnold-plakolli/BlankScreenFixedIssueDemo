f34b1ddf58e85cc5e576a33ad8974960eb289fdc
Arnold Plakolli <arnold.plakolli@pfizer.com>
2022-08-12 16:44:59 +0200

    added ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.arnoldplakolli.blankscreenfixedissuedemo',
   appName: 'BlankScreenFixedIssueDemo',
   webDir: 'www',
   bundledWebRuntime: false
